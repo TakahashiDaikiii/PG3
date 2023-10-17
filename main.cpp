94e188e6a5d787dab84a7d40f3f36391e9822d9a
たかはしだいき <k022g0073@g.ne.jp>
2023-10-17 10:22:45 +0900

    ０１－０１

--- main.cpp
+++ main.cpp
@@ -19,8 +19,25 @@ template<>
 
 char Min<char>(char a, char b)
 {
+	printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
+	return 0;
+}
+
+int main()
+{
+	char a = 'a';
+	char b = 'b';
+
+	printf("%d\n", Min<int>(200, 90));
 
 
+	printf("%f\n", Min<float>(10.0f, 90.0f));
 
+
+	printf("%f\n", Min<double>(15.0f, 9.0f));
+
+	Min<char>(a, b);
+
+	return 0;
 }
 
