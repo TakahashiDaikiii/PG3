30a96f0548f8ab052cbd73755c2d0cd8545a79c7
たかはしだいき <k022g0073@g.ne.jp>
2023-11-14 10:27:15 +0900

    １－３

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef void (*newType)(int*, int*);
+
+void Lottery(int* select, int* num) {
+
+	printf("サイコロの目は%dでした。\n", *num);
+
+	if (*select == 1) {
+		if (*num == 1 || *num == 3 || *num == 5) {
+			printf("正解！！！");
+		}
+		else {
+			printf("不正解！！！");
+		}
+	}
+
+	if (*select == 2) {
+		if (*num == 0 || *num == 2 || *num == 4 || *num == 6) {
+			printf("正解！！！");
+		}
+		else {
+			printf("不正解！！！");
+		}
+	}
+
+}
+
+void SetTimeout(newType calc, int second, int select, int num) {
+
+	Sleep(second * 1000);
+
+	calc(&select, &num);
+
+}
+
+int main() {
+	//ランダム関数
+	srand((unsigned int)time(NULL));
+	newType calc;
+
+	calc = Lottery;
+
+	int num;
+
+	int select = 0;
+
+	printf("半か丁か選べ\n");
+
+	printf("1と入力すると半、2と入力すると丁になります\n");
+
+	scanf_s("%d", &select);
+
+	num = 1 + rand() % 6;
+
+	SetTimeout(calc, 3, select, num);
+
+	return 0;
+
+}
