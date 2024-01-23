1e93e9a94d8e326bed5d7d63e934325b70a700fa
たかはしだいき <k022g0073@g.ne.jp>
2024-01-23 14:26:35 +0900

    s

--- main.cpp
+++ main.cpp
@@ -2,61 +2,55 @@
 #include <Windows.h>
 #include <stdlib.h>
 #include <time.h>
-
-typedef void (*newType)(int*, int*);
-
-void Lottery(int* select, int* num) {
-
-	printf("サイコロの目は%dでした。\n", *num);
-
-	if (*select == 1) {
-		if (*num == 1 || *num == 3 || *num == 5) {
-			printf("正解！！！");
-		}
-		else {
-			printf("不正解！！！");
-		}
-	}
-
-	if (*select == 2) {
-		if (*num == 0 || *num == 2 || *num == 4 || *num == 6) {
-			printf("正解！！！");
-		}
-		else {
-			printf("不正解！！！");
-		}
-	}
-
-}
-
-void SetTimeout(newType calc, int second, int select, int num) {
-
-	Sleep(second * 1000);
-
-	calc(&select, &num);
-
-}
+#include <functional>
 
 int main() {
 	//ランダム関数
 	srand((unsigned int)time(NULL));
-	newType calc;
+	//newType calc;
 
-	calc = Lottery;
+	//calc = Lottery;
+
 
 	int num;
-
 	int select = 0;
 
-	printf("半か丁か選べ\n");
-
+	printf("半か丁を選んでください\n");
 	printf("1と入力すると半、2と入力すると丁になります\n");
-
 	scanf_s("%d", &select);
-
 	num = 1 + rand() % 6;
 
-	SetTimeout(calc, 3, select, num);
+	std::function<void(void)> lottery = [=]() {
+		printf("サイコロの目は%dでした。\n", num);
+
+		if (select == 1) {
+			if (num == 1 || num == 3 || num == 5) {
+				printf("おめでとうごさいます。半です");
+			}
+			else {
+				printf("残念ながら丁になります。");
+			}
+		}
+
+		if (select == 2) {
+			if (num == 0 || num == 2 || num == 4 || num == 6) {
+				printf("おめでとうごさいます。丁です");
+			}
+			else {
+				printf("残念ながら半になります。");
+			}
+		}
+
+		};
+
+	std::function<void(int)> setTimeout = [=](int second) {
+		Sleep(second * 1000);
+
+		lottery();
+
+		};
+
+	setTimeout(3);
 
 	return 0;
 
