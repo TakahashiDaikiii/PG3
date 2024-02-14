63928ea0baea32de23cc698ecb727313f1115212
たかはしだいき <k022g0073@g.ne.jp>
2024-02-14 16:23:38 +0900

    完成

---
+++ Dog.cpp
@@ -0,0 +1,16 @@
+﻿#include "Dog.h"
+#include <stdio.h>
+
+
+void Dog::Cry()
+{
+	printf("one­\n");
+}
+
+Dog::Dog()
+{
+}
+
+Dog::~Dog()
+{
+}
--- main.cpp
+++ main.cpp
@@ -1,42 +1,25 @@
 #include <stdio.h>
-
-
-template <typename T1, typename T2>
-class MinFinder 
-{
-public:
-    // 2Ç¬ÇÃà¯êîÇî‰Ç◊ÅAè¨Ç≥Ç¢ílÇï‘Ç∑
-    auto Min(T1 a, T2 b) -> decltype(a < b ? a : b) {
-        return (a < b) ? a : b;
-    }
-};
+#include "Animal.h"
+#include "Dog.h"
+#include "Cat.h"
 
 
 int main() {
-    // floatå^ÇÃëgÇ›çáÇÌÇπ
-    MinFinder<float, float> floatMinFinder;
-    float floatResult = floatMinFinder.Min(3.25f, 2.45f);
-    printf("Min: 3.25 or 2.45 <float>: %f\n", floatResult);
 
-    // doubleå^ÇÃëgÇ›çáÇÌÇπ
-    MinFinder<double, double> doubleMinFinder;
-    double doubleResult = doubleMinFinder.Min(14.5, 8.2);
-    printf("Min: 14.5 or 8.2 <double>: %lf\n", doubleResult);
+	Animal* animal_[2];
+	for (int i = 0; i < 2; i++) {
+		if (i < 1) {
+			animal_[i] = new Dog;
+		}
+		else {
+			animal_[i] = new Cat;
+		}
+	}
 
-    // intå^Ç∆doubleå^
-    MinFinder<int, double> intDoubleMinFinder;
-    int intDoubleResult = intDoubleMinFinder.Min(6, 7.9);
-    printf("Min: 6 or 7.9 <int>: %d\n", intDoubleResult);
+	for (int i = 0; i < 2; i++) {
+		animal_[i]->Cry();
+	}
+	getchar();
 
-    // intå^Ç∆floatå^
-    MinFinder<int, float> intFloatMinFinder;
-    int intFloatResult = intFloatMinFinder.Min(6, 2.45f);
-    printf("Min: 6 or 2.45 <int>: %d\n", intFloatResult);
-
-    // floatå^Ç∆doubleå^
-    MinFinder<float, double> floatDoubleMinFinder;
-    float floatDoubleResult = floatDoubleMinFinder.Min(3.25f, 7.9);
-    printf("Min: 3.25 or 7.9 <float>: %f\n", floatDoubleResult);
-
-    return 0;
+	return 0;
 }
